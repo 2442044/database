1722a70989a05e580bba5577ec940ca85dc60969
2442044 <g2442044@tcu.ac.jp>
2026-01-15 14:12:46 +0900

    Update host for remote access and include database in git

--- app.py
+++ app.py
@@ -175,4 +175,5 @@ def return_dvd(rental_id):
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    # host='0.0.0.0' を指定することで、同じネットワーク内の他のPCからもアクセス可能になります
+    app.run(debug=True, host='0.0.0.0', port=5000)
